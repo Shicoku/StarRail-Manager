0f0de670d6ecd353f5a73e58bba670361447661c
shicoku <viniseiiti2006@gmail.com>
2025-07-16 16:58:24 +0900

    コンフィグを変更できるように

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Client } from "./client/client";
 import { LightCone } from "./client/light_cone";
 import { Character } from "./client/character";
+import { config } from "./config";
 
 export { Client };
 export { LightCone };
 export { Character };
+export { config };
